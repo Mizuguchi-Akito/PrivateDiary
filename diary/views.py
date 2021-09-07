4b05c8ffa5f6c3f678db9daab4c19196ed4b57f1
Mizuguchi-AKito <ksw2070118@stu.o-hara.ac.jp>
2021-09-07 12:34:21 +0900

    お問い合わせ

--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from.forms import InquiryForm
 # Create your views here.
 def index(request):
     return render(request, 'diary/index.html')
 
 class IndexView(generic.TemplateView):
     template_name="diary/index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm

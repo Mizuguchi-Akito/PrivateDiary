395686889a695e0327f2aa2fc068244cc24ba277
Mizuguchi-Akito <ksw2070118@stu.o-hara.ac.jp>
2021-09-10 12:22:52 +0900

    9/10 進捗

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from . import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('diary.urls')),
     path('accounts/',include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+document_root = settings.MEDIA_ROOT)

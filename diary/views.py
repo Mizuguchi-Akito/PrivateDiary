d6c2707ac826480a8d3069311dd2850140e747d4
Mizuguchi-Akito <akitoooharagakuenn@gmail.com>
2021-09-13 02:18:56 +0000

    進捗 9/13

--- diary/views.py
+++ diary/views.py
@@ -33,6 +33,7 @@ class InquiryView(generic.FormView):
 class DiaryListView(LoginRequiredMixin,generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
+    peginate_by = 2
 
     def get_queryset(self):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
